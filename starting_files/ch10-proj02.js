3c7a46d5f5752bc69dcb7313e9bd19f8fafcafe3
Kyle Duong <kyleduong13@gmail.com>
2024-01-16 12:13:00 -0700

    Everything is hard coded so far

--- starting_files/ch10-proj02.js
+++ starting_files/ch10-proj02.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	
 	const url = 'https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/play.php';
-
+   let playData;
    /*
      To get a specific play, add play name via query string, 
 	   e.g., url = url + '?name=hamlet';
@@ -20,6 +20,35 @@ document.addEventListener("DOMContentLoaded", function() {
        Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
        use built-in Live Preview.
     */
+   function populateActsFilter(acts) {
+      const actList = document.querySelector("#actList");
+      actList.replaceChildren();
+      
+      acts.forEach(act => {
+         let option = document.createElement("option");
+         option.textContent = act.name;
+         option.value = act.name;
+         actList.appendChild(option);
+      });
+      
+      populateScenesFilter(acts[0].scenes)
+   }
+
+   function displayActData(act) {
+      
+   }
+
+   function populateScenesFilter(scenes) {
+      const sceneList = document.querySelector("#sceneList");
+      sceneList.replaceChildren();
+      
+      scenes.forEach(scene => {
+         let option = document.createElement("option");
+         option.textContent = scene.name;
+         option.value = scene.name;
+         sceneList.appendChild(option);
+      });
+   }
 
    async function getPlayList(url) {
       const res = await fetch(url);
@@ -27,11 +56,24 @@ document.addEventListener("DOMContentLoaded", function() {
       return data;
    }
 
+   document.querySelector("#actList").addEventListener("change", (e) => {
+      playData.acts.forEach((act) => {
+         if(e.target.value == act.name) {
+            document.querySelector("h3").textContent = act.name;
+            populateScenesFilter(act.scenes)
+            
+         }
+      })
+   });
+   
    document.querySelector("#playList").addEventListener("change", async (e) => {
       if(e.target.value != 0) {
-         let data = await getPlayList(`${url}?name=${e.target.value}`)
+         playData = await getPlayList(`${url}?name=${e.target.value}`)
+         document.querySelector("h2").textContent = playData.title;
+         document.querySelector("h3").textContent = playData.acts[0].name;
 
-         console.log(data)
+         
+         populateActsFilter(playData.acts)
       }
-   })
+   });
 });
