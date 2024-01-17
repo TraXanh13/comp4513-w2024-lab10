34b2f1f27bf44353a95845f91c14f9598ad0e1d8
Kyle Duong <kyleduong13@gmail.com>
2024-01-16 23:24:14 -0700

    Working through the lab. Its looking pretty good

--- starting_files/ch10-proj02.js
+++ starting_files/ch10-proj02.js
@@ -1,79 +1,70 @@
-
+import Play from "./play-module.js";
 
 document.addEventListener("DOMContentLoaded", function() {
 
 	
 	const url = 'https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/play.php';
-   let playData;
+   let play;
+
    /*
-     To get a specific play, add play name via query string, 
-	   e.g., url = url + '?name=hamlet';
-	 
-	 https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=hamlet
-	 https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=jcaesar
-     
+      To get a specific play, add play name via query string, 
+      e.g., url = url + '?name=hamlet';
+      
+      https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=hamlet
+      https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=jcaesar
    */
-	 
+	
    
-    /* note: you may get a CORS error if you test this locally (i.e., directly from a
-       local file). To work correctly, this needs to be tested on a local web server.  
-       Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
-       use built-in Live Preview.
+   /* note: you may get a CORS error if you test this locally (i.e., directly from a
+      local file). To work correctly, this needs to be tested on a local web server.  
+      Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
+      use built-in Live Preview.
     */
-   function populateActsFilter(acts) {
-      const actList = document.querySelector("#actList");
-      actList.replaceChildren();
-      
-      acts.forEach(act => {
-         let option = document.createElement("option");
-         option.textContent = act.name;
-         option.value = act.name;
-         actList.appendChild(option);
-      });
-      
-      populateScenesFilter(acts[0].scenes)
-   }
-
-   function displayActData(act) {
-      
-   }
-
-   function populateScenesFilter(scenes) {
-      const sceneList = document.querySelector("#sceneList");
-      sceneList.replaceChildren();
-      
-      scenes.forEach(scene => {
-         let option = document.createElement("option");
-         option.textContent = scene.name;
-         option.value = scene.name;
-         sceneList.appendChild(option);
-      });
-   }
 
    async function getPlayList(url) {
       const res = await fetch(url);
       const data = await res.json();
       return data;
    }
-
-   document.querySelector("#actList").addEventListener("change", (e) => {
-      playData.acts.forEach((act) => {
-         if(e.target.value == act.name) {
-            document.querySelector("h3").textContent = act.name;
-            populateScenesFilter(act.scenes)
-            
-         }
-      })
-   });
    
    document.querySelector("#playList").addEventListener("change", async (e) => {
       if(e.target.value != 0) {
-         playData = await getPlayList(`${url}?name=${e.target.value}`)
-         document.querySelector("h2").textContent = playData.title;
-         document.querySelector("h3").textContent = playData.acts[0].name;
-
-         
-         populateActsFilter(playData.acts)
+         play = new Play(await getPlayList(`${url}?name=${e.target.value}`));
+         play.populateActFilter();
+         play.populatePersonaFilter()
+         play.displayPlay();
       }
    });
+   
+   document.querySelector("#actList").addEventListener("change", (e) => {
+      let act = play.acts.find((act) => {
+         if(act.name === e.target.value) 
+            return true;
+      })
+
+      act.populateSceneFilter();
+      play.displayPlay(e.target.value);
+   });
+
+   document.querySelector("#sceneList").addEventListener("change", (e) => {
+      play.displayPlay(e.target.previousElementSibling.value, e.target.value)
+   })
+
+   function filterAct(speeches, textSearch){
+      let lines = document.querySelector("#lines");
+      lines.replaceChildren(lines, ...speeches);
+   }
+
+   document.querySelector("#btnHighlight").addEventListener("click", () => {
+      const player = document.querySelector("#playerList").value;
+      const highlight = document.querySelector("#txtHighlight").value;
+      let speeches = Array.from(document.querySelectorAll(".speech"))
+
+
+      if(player != 0) {
+         speeches = speeches.filter((speech) => speech.querySelector("span").textContent === player)
+      }
+
+      filterAct(speeches, highlight)
+   })
 });
--- starting_files/play-module.js
+++ starting_files/play-module.js
@@ -1,2 +1,151 @@
 /* In this module, create three classes: Play, Act, and Scene. */
+class Play {
+    constructor(play){
+        this.title = play.title;
+        this.personas = play.persona;
+        this.acts = [];
+        
+        play.acts.forEach(act => {
+            this.acts.push(new Act(act))
+        });
+    }
 
+    populateActFilter() {
+        const actList = document.querySelector("#actList");
+        actList.replaceChildren();
+        
+        this.acts.forEach(act => {
+            let option = document.createElement("option");
+            option.textContent = act.name;
+            option.value = act.name;
+            actList.appendChild(option);
+        });
+
+        this.acts[0].populateSceneFilter();
+    }
+
+    populatePersonaFilter() {
+        const playerList = document.querySelector("#playerList");
+        let playerListPlaceholder = playerList.firstElementChild;
+
+        playerList.replaceChildren();
+        playerList.appendChild(playerListPlaceholder);
+
+        this.personas.forEach(persona => {
+            let option = document.createElement("option");
+            option.textContent = persona.player;
+            option.value = persona.player;
+            playerList.appendChild(option);
+        });
+    }
+
+    displayPlay(act = "ACT I", scene = "SCENE I") {
+        const currAct = this.acts.find((a) => {
+            if(a.name === act)
+                return true;
+        })
+
+        const playHere = document.querySelector("#playHere");
+        playHere.replaceChildren();
+        
+        let playTitle = document.createElement("h2");
+        playTitle.textContent = this.title;
+        
+        let actHere = document.createElement("article");
+        actHere.setAttribute("id", "actHere");
+        
+        let h3 = document.createElement("h3");
+        h3.textContent = currAct.name;
+        
+        playHere.appendChild(playTitle);
+        playHere.appendChild(actHere);
+        actHere.appendChild(h3);
+        actHere.appendChild(currAct.getSceneMarkup(scene));
+    }
+}
+    
+    class Act {
+        constructor(act){
+            this.name = act.name;
+            this.scenes = [];
+            
+            act.scenes.forEach(scene => {
+                this.scenes.push(new Scene(scene));
+        })
+    }
+    
+    populateSceneFilter(){
+        const sceneList = document.querySelector("#sceneList");
+        sceneList.replaceChildren();
+        
+        this.scenes.forEach(scene => {
+            let option = document.createElement("option");
+            option.textContent = scene.name;
+            option.value = scene.name;
+            sceneList.appendChild(option);
+        });
+    }
+
+    getSceneMarkup(scene) {
+        const currScene = this.scenes.find((s) => {
+            if(s.name === scene)
+                return true;
+        });
+
+        let sceneHere = document.createElement("div");
+        sceneHere.setAttribute("id", "sceneHere");
+
+        let h4 = document.createElement("h4");
+        h4.textContent = currScene.name;
+
+        let title = document.createElement("p");
+        title.setAttribute("class", "title");
+        title.textContent = currScene.title;
+
+        let direction = document.createElement("p");
+        direction.setAttribute("class", "direction");
+        direction.textContent = currScene.stageDirection;
+
+        sceneHere.appendChild(h4);
+        sceneHere.appendChild(title);
+        sceneHere.appendChild(direction);
+        
+        sceneHere.appendChild(currScene.getLinesMarkup());
+
+        return sceneHere;
+    }
+}
+
+class Scene {
+    constructor(scene){
+        this.name = scene.name;
+        this.title = scene.title;
+        this.stageDirection = scene.stageDirection;
+        this.speeches = scene.speeches;
+    }
+
+    getLinesMarkup(){
+        const linesMarkup = document.createElement("div")
+        linesMarkup.setAttribute("id", "lines");
+        
+        this.speeches.forEach((speech) => {
+            let createdSpeech = document.createElement("div");
+            createdSpeech.setAttribute("class", "speech");
+            
+            let speaker = document.createElement("span");
+            speaker.textContent = speech.speaker;
+            
+            createdSpeech.appendChild(speaker);
+
+            speech.lines.forEach(l => {
+                let p = document.createElement("p");
+                p.textContent = l;
+                createdSpeech.appendChild(p);
+            })
+            linesMarkup.appendChild(createdSpeech)
+        })
+        return linesMarkup;
+    }
+}
+
+export default Play;
